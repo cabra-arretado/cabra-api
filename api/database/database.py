1706b50f84932dcdc7e56ead7cc948e56993dda3
Felipe <felipemouradev@gmail.com>
2023-07-20 17:17:35 -0500

    start implementing database classes

---
+++ api/database/database.py
@@ -0,0 +1,11 @@
+from utils import get_env
+
+import sqlalchemy
+
+test_env = {
+"DB_HOST": "localhost",
+"DB_PORT": 3306,
+"DB_USERNAME": "root",
+"DB_PASSWORD": "password",
+"DB_DATABASE": "customers",
+}
