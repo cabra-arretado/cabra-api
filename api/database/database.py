e10308362a9468e613df893550ddc41bf5b38a02
Felipe <felipemouradev@gmail.com>
2023-07-24 15:43:48 -0500

    add seeding

--- api/database/database.py
+++ api/database/database.py
@@ -9,3 +9,11 @@ test_env = {
 "DB_PASSWORD": "password",
 "DB_DATABASE": "customers",
 }
+env = get_env(test_env)
+
+# Create a connection to the database
+engine = sqlalchemy.create_engine(f"mysql+pymysql://{env.get('DB_USERNAME')}:{env.get('DB_PASSWORD')}@{env.get('DB_HOST')}:{env.get('DB_PORT')}/{env.get('DB_DATABASE')}")
+
+# Create a table
+engine.execute("CREATE TABLE IF NOT EXISTS customers (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255)")
+
---
+++ mysql/seed.sql
@@ -0,0 +1,9 @@
+CREATE TABLE users (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+INSERT INTO users (name, email) VALUES ('John Doe', 'johndoe@example.com');
+INSERT INTO users (name, email) VALUES ('Jane Doe', 'janedoe@example.com');
